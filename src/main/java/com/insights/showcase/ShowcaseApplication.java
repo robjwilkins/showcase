c6a78ed0c54d1445348b06f57351acf720aa4cba
Robin Wilkins <robjwilkins@gmail.com>
2020-07-29 12:12:45 +0100

    initial project creation

---
+++ src/main/java/com/insights/showcase/ShowcaseApplication.java
@@ -0,0 +1,12 @@
+package com.insights.showcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowcaseApplication.class, args);
+    }
+}
