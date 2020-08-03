7c95b2692e7df72afb68d6a01a086110225097f0
Robin Wilkins <robjwilkins@gmail.com>
2020-08-03 17:30:44 +0100

    Add spock data driven test example.
    Added:
    - dependency on groovy, and spock framework for testing.
    - Spock test for the GreetingController
    - Filter adding a trace-id header to http responses

--- src/main/java/com/wilkins/showcase/ShowcaseApplication.java
+++ src/main/java/com/wilkins/showcase/ShowcaseApplication.java
@@ -2,11 +2,22 @@ package com.wilkins.showcase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
+import java.util.UUID;
+import java.util.function.Supplier;
+
+@ServletComponentScan
 @SpringBootApplication
 public class ShowcaseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShowcaseApplication.class, args);
+    }
+
+    @Bean
+    public Supplier<String> randomStringSupplier() {
+        return () -> UUID.randomUUID().toString();
     }
 }
