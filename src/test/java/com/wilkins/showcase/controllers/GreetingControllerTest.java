3b8c716e511c1891296e9c3c60993286bc38bbaa
Robin Wilkins <r.wilkins@waracle.com>
2020-09-28 17:02:19 +0100

    Add demo of job scheduling using quartz
    The GreetingController is extended, so it now allows a POST which schedules a job to
    run at a set frequency. The job just logs the Greeting to the console
    
    Quartz is a framework which allows Jobs to be scheduled to run asynchronously, at a fixed
    frequency or point in time. This is useful if you want to allow the API user to
    schedule a task to be performed at a set point in the future
    
    The job schedule is persisted in an h2 database, so they will live beyond the lifespan
    of an application run. I.e. the application can be restarted, and the running jobs will
    restart. The database schema is created using Flyway
    
    Jira: n/a

--- src/test/java/com/wilkins/showcase/controllers/GreetingControllerTest.java
+++ src/test/java/com/wilkins/showcase/controllers/GreetingControllerTest.java
@@ -1,20 +1,30 @@
 package com.wilkins.showcase.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.quartz.Scheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 public class GreetingControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @MockBean
+    Scheduler scheduler;
 
     @Test
     void returnsGreeting() throws Exception {
@@ -24,4 +34,11 @@ public class GreetingControllerTest {
                 .andExpect(jsonPath("$.name", is("world")));
     }
 
+    @Test
+    void scheduleGreeting() throws Exception {
+        mockMvc.perform(post("/greeting"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(notNullValue()));
+        verify(scheduler).scheduleJob(any(), any());
+    }
 }
