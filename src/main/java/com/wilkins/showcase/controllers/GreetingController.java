fc0b6ea7a58a1460f4f39495ba1999bc8a18a0bd
Robin Wilkins <r.wilkins@waracle.com>
2020-10-22 14:18:29 +0100

    Add an endpoint to download a zip created from other files
    The new endpoint allows download of a dynamically created zip file. Proof of Concept
    looking at a means of generating a zip for download containing a number of other

--- src/main/java/com/wilkins/showcase/controllers/GreetingController.java
+++ src/main/java/com/wilkins/showcase/controllers/GreetingController.java
@@ -1,10 +1,21 @@
 package com.wilkins.showcase.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
+import java.io.*;
+import java.util.List;
+import java.util.UUID;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import static org.springframework.http.ResponseEntity.ok;
 import static org.springframework.util.StringUtils.isEmpty;
 
 @Slf4j
@@ -13,7 +24,7 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public Greeting getGreeting(@RequestParam(name = "salutation", required = false) String salutationParam,
-                               @RequestParam(name = "name", required = false) String nameParam) {
+                                @RequestParam(name = "name", required = false) String nameParam) {
 
         log.info("A greeting was requested");
 
@@ -29,5 +40,42 @@ public class GreetingController {
         log.info("Greeting returned: {}", greeting);
 
         return greeting;
+    }
+
+    @GetMapping(value = "/something", produces = "application/zip")
+    public ResponseEntity<StreamingResponseBody> download() {
+        StreamingResponseBody body = outputStream -> {
+            var zip = new ZipOutputStream(outputStream);
+            files().forEach(file -> addToZip(file, zip));
+            zip.close();
+        };
+        return ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition
+                        .builder("zipfile").filename("something.zip").build().toString())
+                .body(body);
+    }
+
+    private List<String> files() {
+        return List.of("src/main/resources/test-doc-one.pdf",
+                "src/main/resources/test-doc-two.pdf");
+    }
+
+    private void addToZip(String file, ZipOutputStream zip) {
+        var zipEntry = new ZipEntry(name());
+        try {
+            zip.putNextEntry(zipEntry);
+            readFileAndSendTo(zip, file);
+        } catch (IOException e) {
+            log.error("Exception adding files to zip", e);
+        }
+    }
+
+    private void readFileAndSendTo(OutputStream outputStream, String fileName) throws IOException {
+        InputStream inputStream = new FileInputStream(new File(fileName));
+        inputStream.transferTo(outputStream);
+    }
+
+    private String name() {
+        return UUID.randomUUID() + ".pdf";
     }
 }
