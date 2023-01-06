76c86c6a9a55c37b777e1aab9b02c7ade8789877
Robin Wilkins <robin@cleancoders.net>
2023-01-06 16:32:43 +0000

    Update README
    Add more information to the README covering the purpose of the open pull requests
    for demonstrating the addition of useful features to the plain springboot app

--- src/main/java/com/wilkins/showcase/controllers/GreetingController.java
+++ src/main/java/com/wilkins/showcase/controllers/GreetingController.java
@@ -3,15 +3,17 @@ package com.wilkins.showcase.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/greeting")
 public class GreetingController {
 
-    private final Logger log = LoggerFactory.getLogger(GreetingController.class);
+    private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
 
-    @GetMapping("/greeting")
+    @GetMapping
     public Greeting getGreeting(@RequestParam(name = "salutation", required = false, defaultValue = "hello") String salutationParam,
                                @RequestParam(name = "name", required = false, defaultValue = "world") String nameParam) {
 
