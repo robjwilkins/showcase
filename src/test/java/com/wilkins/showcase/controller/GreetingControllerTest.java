8f6703c9a8f6cfefad604b882c24d117de7b02a0
Robin Wilkins <robjwilkins@gmail.com>
2025-11-28 23:46:33 +0000

    Upgrade project to Springboot 4

--- src/test/java/com/wilkins/showcase/controller/GreetingControllerTest.java
+++ src/test/java/com/wilkins/showcase/controller/GreetingControllerTest.java
@@ -1,14 +1,14 @@
 package com.wilkins.showcase.controller;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = GreetingController.class)
 public class GreetingControllerTest {
